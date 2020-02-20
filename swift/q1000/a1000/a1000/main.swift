ebda8fa7b23a5f2d1bd59107194d0db6d011580d
paikwiki <paikwiki@gmail.com>
2020-02-20 18:53:07 +0900

    Solve 5 quizzes with Swift

---
+++ swift/q1000/a1000/a1000/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  a1000
+//
+//  Created by Changhyun Paik on 2020/02/20.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import Foundation
+
+var inp = readLine();
+let arr = inp!.components(separatedBy: " ")
+let result = Int(arr[0])! + Int(arr[1])!
+print(result)
