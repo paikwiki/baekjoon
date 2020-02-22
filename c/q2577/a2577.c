16ab2acd890a0047288f74edc04a145ae97838d1
paikwiki <paikwiki@gmail.com>
2020-02-22 21:36:04 +0900

    Solve q2557 with C

--- c/q2577/a2577.c
+++ c/q2577/a2577.c
@@ -4,4 +4,22 @@
 
 int main(void)
 {
+    int arr[10];
+    int a;
+    int b;
+    int c;
+    int abc;
+
+    for (a = 0; a < 10; a++)
+        arr[a] = 0;
+    scanf("%d %d %d", &a, &b, &c);
+    abc = a * b * c;
+    while (abc > 0)
+    {
+        arr[abc % 10] += 1;
+        abc /= 10;
+    }
+    for (a = 0; a < 10; a++)
+        printf("%d\n", arr[a]);
+    return (0);
 }
