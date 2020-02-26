4d0246cef6d8080028ea779d5138b2c0940f03b8
paikwiki <paikwiki@gmail.com>
2020-02-26 19:21:26 +0900

    Save a2986 with C, but not submit yet

---
+++ c/q2986/a2986.c
@@ -0,0 +1,24 @@
+/* https://www.acmicpc.net/problem/2986 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+	int n;
+
+	scanf("%d", &n);
+
+	j = 1;
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			j = n / i;
+			break;
+		}
+	}
+	printf("%d\n", n - j);
+	return (0);
+}
