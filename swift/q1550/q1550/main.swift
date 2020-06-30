7c2e06c5b6ed3356bb6b8c1348844ac740965e90
paikwiki <paikwiki@gmail.com>
2020-06-30 19:01:09 +0900

    Solve q1550 with Swift

---
+++ swift/q1550/q1550/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  q1550
+//
+//  Created by Changhyun Paik on 2020/06/30.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+let result = Int(readLine()!, radix: 16)!
+print(result)
